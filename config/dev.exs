5172b5f546c3257144aad332554f8a22d1bf3660
Christophe De Troyer <christophe.detroyer@gmail.com>
2026-07-03 12:46:31 +0200

    dev on 0.0.0.0

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,7 @@ config :poly_bot, PolyBot.Repo,
 config :poly_bot, PolyBotWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}],
+  http: [ip: {0, 0, 0, 0}],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
